b2e18a53b2a82b983dd8668cde5035eab5c30444
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-04-20 23:36:50 +0500

    Готов класс Name

---
+++ cli.php
@@ -0,0 +1,9 @@
+<?php
+
+use Eastap\PhpBlog\User\Name;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$name = new Name('Ben', 'Gann');
+
+print $name;
---
+++ src/User/Name.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Eastap\PhpBlog\User;
+
+class Name {
+  private string $firstname;
+  private string $lastname;
+
+  public function __construct(
+    $firstname,
+    $lastname
+  ) {
+    $this->firstname = $firstname;
+    $this->lastname = $lastname;
+  }
+
+  public function __toString()
+  {
+    return $this->firstname ." ". $this->lastname;
+  }
+}
