893c6faa22985badd52063ad54133082cd9ce9bc
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-04-22 22:30:31 +0500

    в cli.php готова обработка

--- cli.php
+++ cli.php
@@ -1,49 +1,39 @@
 <?php
 
 namespace Eastap\PhpBlog\Blog;
-// use \Eastap\PhpBlog\User\User;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $faker = \Faker\Factory::create();
 
-$user = new User(
-  $faker->unique()->numberBetween(1, 1000),
-  $faker->firstName(null),
-  $faker->lastName(null)
-);
+switch($argv[1]) {
+  case 'user':
+    print new User(
+        $faker->unique()->numberBetween(1, 1000),
+        $faker->firstName(null),
+        $faker->lastName(null)
+      );
+    break;
 
-print $user . PHP_EOL;
-print PHP_EOL;
-print $user->getId() . PHP_EOL;;
-print $user->getFirstName() . PHP_EOL;;
-print $user->getLastName() . PHP_EOL;;
-print PHP_EOL;
+    case 'post':
+      print new Post(
+          $faker->unique()->numberBetween(1, 1000),
+          new User(
+            $faker->unique()->numberBetween(1, 1000),
+            $faker->firstName(null),
+            $faker->lastName(null)
+          ),
+          $faker->sentence(rand(1, 5)),
+          $faker->paragraph(rand(2, 5))
+        );
+      break;
 
-$post = new Post(
-  $faker->unique()->numberBetween(1, 1000),
-  $user,
-  $faker->sentence(rand(1, 5)),
-  $faker->paragraph(rand(2, 5))
-);
-
-print $post . PHP_EOL;
-print PHP_EOL;
-print $post->getId() . PHP_EOL;
-print $post->getAuthor() . PHP_EOL;
-print $post->getTitle() . PHP_EOL;
-print $post->getText() . PHP_EOL;
-
-$comment = new Comment(
-  $faker->unique()->numberBetween(1, 1000),
-  $faker->unique()->numberBetween(1, 1000),
-  $faker->unique()->numberBetween(1, 1000),
-  $faker->paragraph(rand(1, 3))
-);
-
-print $comment . PHP_EOL;
-print PHP_EOL;
-print $comment->getId() . PHP_EOL;
-print $comment->getPostId() . PHP_EOL;
-print $comment->getUserId() . PHP_EOL;
-print $comment->getText() . PHP_EOL;
+      case 'comment':
+        print new Comment(
+            $faker->unique()->numberBetween(1, 1000),
+            $faker->unique()->numberBetween(1, 1000),
+            $faker->unique()->numberBetween(1, 1000),
+            $faker->paragraph(rand(1, 3))
+          );
+        break;
+}
