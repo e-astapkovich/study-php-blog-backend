b858eeb8fc1fb56cf72d1a761f9e2baec5753e46
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-04-22 21:50:46 +0500

    Готов класс Post

--- cli.php
+++ cli.php
@@ -14,6 +14,22 @@ $user = new User(
 );
 
 print $user . PHP_EOL;
+print PHP_EOL;
 print $user->getId() . PHP_EOL;;
 print $user->getFirstName() . PHP_EOL;;
 print $user->getLastName() . PHP_EOL;;
+print PHP_EOL;
+
+$post = new Post(
+  $faker->unique()->numberBetween(1, 1000),
+  $user,
+  $faker->sentence(rand(1, 5)),
+  $faker->paragraph(rand(2, 5))
+);
+
+print $post . PHP_EOL;
+print PHP_EOL;
+print $post->getId() . PHP_EOL;
+print $post->getAuthor() . PHP_EOL;
+print $post->getTitle() . PHP_EOL;
+print $post->getText() . PHP_EOL;
