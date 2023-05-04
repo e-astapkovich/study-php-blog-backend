c8580205df773766fe1b1f137549a6a234232753
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-04 23:06:04 +0500

    Добавил в cli.php подключение к БД

--- cli.php
+++ cli.php
@@ -2,38 +2,44 @@
 
 namespace Eastap\PhpBlog\Blog;
 
+use \PDO;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $faker = \Faker\Factory::create();
 
-switch($argv[1]) {
-  case 'user':
-    print new User(
-        $faker->word(),
-        $faker->firstName(null),
-        $faker->lastName(null)
-      );
-    break;
+$pdo = new PDO('sqlite:blog.sqlite', null, null, [
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
 
-    case 'post':
-      print new Post(
-          $faker->unique()->numberBetween(1, 1000),
-          new User(
-            $faker->word(),
-            $faker->firstName(null),
-            $faker->lastName(null)
-          ),
-          $faker->sentence(rand(1, 5)),
-          $faker->paragraph(rand(2, 5))
-        );
-      break;
+// switch($argv[1]) {
+//   case 'user':
+//     print new User(
+//         $faker->word(),
+//         $faker->firstName(null),
+//         $faker->lastName(null)
+//       );
+//     break;
 
-      case 'comment':
-        print new Comment(
-            $faker->unique()->numberBetween(1, 1000),
-            $faker->unique()->numberBetween(1, 1000),
-            $faker->unique()->numberBetween(1, 1000),
-            $faker->paragraph(rand(1, 3))
-          );
-        break;
-}
+//     case 'post':
+//       print new Post(
+//           $faker->unique()->numberBetween(1, 1000),
+//           new User(
+//             $faker->word(),
+//             $faker->firstName(null),
+//             $faker->lastName(null)
+//           ),
+//           $faker->sentence(rand(1, 5)),
+//           $faker->paragraph(rand(2, 5))
+//         );
+//       break;
+
+//       case 'comment':
+//         print new Comment(
+//             $faker->unique()->numberBetween(1, 1000),
+//             $faker->unique()->numberBetween(1, 1000),
+//             $faker->unique()->numberBetween(1, 1000),
+//             $faker->paragraph(rand(1, 3))
+//           );
+//         break;
+// }
