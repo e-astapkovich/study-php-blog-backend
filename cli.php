c70d46a8450d14720129d6dec2941895f2ea2e6f
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-09 16:26:03 +0500

    из cli.php убрал закомментированный код

--- cli.php
+++ cli.php
@@ -20,18 +20,8 @@ $pdo = new PDO('sqlite:blog.sqlite', null, null, [
 ]);
 
 $userRepo = new SqliteUserRepository($pdo);
-
-// $pirate = new User(UUID::random(), 'pirate', 'Billy', 'Bons');
-// $repo->save($pirate);
-
-try {
-  // echo $repo->get(new UUID('3bed1a29-6737-45ff-81f2-7366f56498a7'));
-echo $userRepo->getByLogin('pirate') . PHP_EOL;
-} catch (AppException $e) {
-  echo "пользователь не найден";
-}
-
 $postRepo = new SqlitePostRepository($pdo);
+$commentRepo = new SqliteCommentRepository($pdo);
 
 try {
   echo $postRepo->get(new UUID('881a75e6-5db9-4106-bb1c-94a84058594f')) . PHP_EOL;
@@ -39,46 +29,8 @@ try {
   echo $e->getMessage();
 }
 
-$commentRepo = new SqliteCommentRepository($pdo);
-
 try {
   echo $commentRepo->get(new UUID('616f7121-89d0-4f38-b356-31746394d41a')) . PHP_EOL;
 } catch (AppException $e) {
   echo $e->getMessage();
 }
-
-
-
-
-
-// switch($argv[1]) {
-//   case 'user':
-//     print new User(
-//         $faker->word(),
-//         $faker->firstName(null),
-//         $faker->lastName(null)
-//       );
-//     break;
-
-//     case 'post':
-//       print new Post(
-//           $faker->unique()->numberBetween(1, 1000),
-//           new User(
-//             $faker->word(),
-//             $faker->firstName(null),
-//             $faker->lastName(null)
-//           ),
-//           $faker->sentence(rand(1, 5)),
-//           $faker->paragraph(rand(2, 5))
-//         );
-//       break;
-
-//       case 'comment':
-//         print new Comment(
-//             $faker->unique()->numberBetween(1, 1000),
-//             $faker->unique()->numberBetween(1, 1000),
-//             $faker->unique()->numberBetween(1, 1000),
-//             $faker->paragraph(rand(1, 3))
-//           );
-//         break;
-// }
