ebde0b5987ee26fd465c1a188e0ee493eee5ee93
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-15 22:39:10 +0500

    Добавил файл http.php - точка входа для http запросов

---
+++ http.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Eastap\PhpBlog;
+
+use Eastap\PhpBlog\Http\Request;
+use Eastap\PhpBlog\Http\SuccessResponse;
+use Eastap\PhpBlog\Http\ErrorResponse;
+use Eastap\PhpBlog\Exceptions\HttpException;
+
+require_once __DIR__ . '/vendor/autoload.php';
