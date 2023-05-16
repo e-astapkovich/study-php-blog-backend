d508454c2cbed2c008c12a760e926b7e080c92cc
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-16 23:07:16 +0500

    Добавил в http.php роутер - массив

--- http.php
+++ http.php
@@ -2,6 +2,7 @@
 
 namespace Eastap\PhpBlog;
 
+use Eastap\PhpBlog\Exceptions\AppException;
 use Eastap\PhpBlog\Http\Request;
 use Eastap\PhpBlog\Http\SuccessResponse;
 use Eastap\PhpBlog\Http\ErrorResponse;
@@ -16,6 +17,35 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $request = new Request($_GET, $_SERVER);
 
+try {
+    $path = $request->path();
+} catch (HttpException $e) {
+    (new ErrorResponse)->send();
+    return;
+}
+
+$routes = [
+    '/users/show' => new FindByLogin(
+        new SqliteUserRepository(
+            new PDO('sqlite:blog.sqlite', null, null, [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ])
+        )
+    ),
+    '/posts/show' => new FindPostByUuid(
+        new SqlitePostRepository(
+            new PDO('sqlite:blog.sqlite', null, null, [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ])
+        ),
+        new SqliteUserRepository(
+            new PDO('sqlite:blog.sqlite', null, null, [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ])
+        )
+    )
+];
+
 // $action = new FindByLogin(
 //     new SqliteUserRepository(
 //         new PDO('sqlite:blog.sqlite', null, null, [
@@ -24,17 +54,31 @@ $request = new Request($_GET, $_SERVER);
 //     )
 // );
 
-$action = new FindPostByUuid(
-    new SqlitePostRepository(
-        new PDO('sqlite:blog.sqlite', null, null, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ])
-    ),
-    new SqliteUserRepository(
-        new PDO('sqlite:blog.sqlite', null, null, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ])
-    )
-);
+// $action = new FindPostByUuid(
+//     new SqlitePostRepository(
+//         new PDO('sqlite:blog.sqlite', null, null, [
+//             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+//         ])
+//     ),
+//     new SqliteUserRepository(
+//         new PDO('sqlite:blog.sqlite', null, null, [
+//             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+//         ])
+//     )
+// );
 
-$action->handle($request)->send();
+// $action->handle($request)->send();
+
+if (!array_key_exists($path, $routes)) {
+    (new ErrorResponse('Not found'))->send();
+}
+
+$action = $routes[$path];
+
+try {
+    $response = $action->handle($request);
+} catch (AppException $e) {
+    (new ErrorResponse($e->getMessage()))->send();
+}
+
+$response->send();
