b1ee3d15c48d1b2f0631ea6ef92bdeb954a01fac
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-22 23:58:02 +0500

    Добавил в DIContainer возможность добавления заранее предопределенных объектов

--- src/Container/DIContainer.php
+++ src/Container/DIContainer.php
@@ -8,14 +8,17 @@ class DIContainer
 {
     private array $resolvers = [];
 
-    public function bind(string $type, string $class) {
-        $this->resolvers[$type] = $class;
+    public function bind(string $type, $resolver):void {
+        $this->resolvers[$type] = $resolver;
     }
 
     public function get(string $type): object {
 
         if (array_key_exists($type, $this->resolvers)) {
             $typeToCreate = $this->resolvers[$type];
+            if (is_object($typeToCreate)) {
+                return $typeToCreate;
+            }
             return $this->get($typeToCreate);
         }
 
