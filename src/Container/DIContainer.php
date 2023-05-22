957ccbf9e5169269700b84584e07ce9e9131dd2a
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-22 23:20:46 +0500

    В DIContainer добавил метод сопоставления интерфейса и класса

--- src/Container/DIContainer.php
+++ src/Container/DIContainer.php
@@ -6,7 +6,19 @@ use Eastap\PhpBlog\Exceptions\NotFoundException;
 
 class DIContainer
 {
+    private array $resolvers = [];
+
+    public function bind(string $type, string $class) {
+        $this->resolvers[$type] = $class;
+    }
+
     public function get(string $type): object {
+
+        if (array_key_exists($type, $this->resolvers)) {
+            $typeToCreate = $this->resolvers[$type];
+            return $this->get($typeToCreate);
+        }
+
         if (!class_exists($type)) {
             throw new NotFoundException("Cannot resolve type: $type");
         }
