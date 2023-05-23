735cc01b2d4a127234d7b4c43409135a2fd399d1
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-24 00:40:51 +0500

    Обновил точку входа http.php

--- http.php
+++ http.php
@@ -16,7 +16,7 @@ use Eastap\PhpBlog\Repositories\SqlitePostRepository;
 use Eastap\PhpBlog\Repositories\SqliteCommentRepository;
 use PDO;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/bootstrap.php';
 
 $request = new Request(
     $_GET,
@@ -40,51 +40,15 @@ try {
 
 $routes = [
     'GET' => [
-        '/users/show' => new FindByLogin(
-            new SqliteUserRepository(
-                new PDO('sqlite:blog.sqlite', null, null, [
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ])
-            )
-        ),
-        '/posts/show' => new FindPostByUuid(
-            new SqlitePostRepository(
-                new PDO('sqlite:blog.sqlite', null, null, [
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ])
-            ),
-            new SqliteUserRepository(
-                new PDO('sqlite:blog.sqlite', null, null, [
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ])
-            )
-        )
+        '/users/show' => FindByLogin::class,
+        '/posts/show' => FindPostByUuid::class
     ],
     'POST' => [
-        '/users/create' => null,
-        '/posts/create' => new CreatePost(
-            new SqlitePostRepository(
-                new PDO('sqlite:blog.sqlite', null, null, [
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ])
-            )
-        ),
-        '/comments/create' => new CreateComment(
-            new SqliteCommentRepository(
-                new PDO('sqlite:blog.sqlite', null, null, [
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ])
-            )
-        ),
+        '/posts/create' => CreatePost::class,
+        '/comments/create' => CreateComment::class,
     ],
     'DELETE' => [
-        '/posts' => new DeletePost(
-            new SqlitePostRepository(
-                new PDO('sqlite:blog.sqlite', null, null, [
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ])
-            )
-        )
+        '/posts' => DeletePost::class
     ]
 ];
 
@@ -96,7 +60,7 @@ if (!array_key_exists($path, $routes[$method])) {
     (new ErrorResponse("Path is not defined: $path"))->send();
 };
 
-$action = $routes[$method][$path];
+$action = $container->get($routes[$method][$path]);
 
 try {
     $response = $action->handle($request);
