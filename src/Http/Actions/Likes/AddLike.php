9a56ac8afe162a12b46eefbbb47637705dba9c2f
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-25 00:26:41 +0500

    В HTTP действие AddLike добавил проверку ставил ли уже этот пользователь лайк этой статье

--- src/Http/Actions/Likes/AddLike.php
+++ src/Http/Actions/Likes/AddLike.php
@@ -11,22 +11,33 @@ use Eastap\PhpBlog\Http\ErrorResponse;
 use Eastap\PhpBlog\Exceptions\HttpException;
 use Eastap\PhpBlog\Blog\Like;
 use Eastap\PhpBlog\Exceptions\AppException;
+use Eastap\PhpBlog\Exceptions\NoLikesException;
 use Eastap\PhpBlog\UUID;
 
 class AddLike implements ActionInterface
 {
     public function __construct(
         private LikeRepositoryInterface $likeRepository
-    )
-    {}
+    ) {
+    }
 
-    public function handle(Request $request): Response {
+    public function handle(Request $request): Response
+    {
         try {
             $postId = $request->JsonBodyField('post_id');
             $userId = $request->JsonBodyField('user_id');
         } catch (HttpException $e) {
             return new ErrorResponse($e->getMessage());
         }
+
+        try {
+            $postLikes = $this->likeRepository->getByPostUuid(new UUID($postId));
+            foreach ($postLikes as $like) {
+                if ($like['user_uuid'] == $userId) {
+                    return new ErrorResponse('Пользователь уже ставил лайк этой статье.');
+                }
+            }
+        } catch (NoLikesException $e) {}
 
         $newLikeUuid = UUID::random();
 
@@ -46,6 +57,5 @@ class AddLike implements ActionInterface
             'result' => 'like added',
             'uuid' => (string)$newLikeUuid
         ]);
-
     }
 }
