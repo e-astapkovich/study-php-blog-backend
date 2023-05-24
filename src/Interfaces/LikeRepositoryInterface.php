c0af72d3105a7959aceb1f940a61ff7eac0e046c
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-24 23:25:59 +0500

    Сделал Sqlite-реализацию репозитория лайков

--- src/Interfaces/LikeRepositoryInterface.php
+++ src/Interfaces/LikeRepositoryInterface.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Eastap\PhpBlog\Interfaces;
+
 use Eastap\PhpBlog\UUID;
+use Eastap\PhpBlog\Blog\Like;
 
 interface LikeRepositoryInterface
 {
-    public function save(): void;
+    public function save(Like $like): void;
     public function getByPostUuid(UUID $postUuid): array;
 }
