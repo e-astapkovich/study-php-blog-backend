0dce733dd3b56a826b310503491dc621a75e5288
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-27 16:35:24 +0500

    Добавил логирование в CreateUserCommand

--- src/Commands/CreateUserCommand.php
+++ src/Commands/CreateUserCommand.php
@@ -8,29 +8,36 @@ use Eastap\PhpBlog\Exceptions\UserNotFoundException;
 use Eastap\PhpBlog\Commands\Arguments;
 use Eastap\PhpBlog\Blog\User;
 use Eastap\PhpBlog\UUID;
+use Psr\Log\LoggerInterface;
 
 class CreateUserCommand
 {
     public function __construct(
-        private UserRepositoryInterface $userRepository
+        private UserRepositoryInterface $userRepository,
+        private LoggerInterface $logger
     ) {
     }
 
     public function handle(Arguments $arguments): void
     {
+        $this->logger->info('Create user command started.');
         $login = $arguments->get('login');
         if ($this->userExists($login)) {
             throw new CommandException("User already exists: $login");
         }
 
+        $uuid = UUID::random();
+
         $user = new User(
-            UUID::random(),
+            $uuid,
             $arguments->get('login'),
             $arguments->get('first_name'),
             $arguments->get('last_name')
 
         );
         $this->userRepository->save($user);
+
+        $this->logger->info("New user created: $uuid");
     }
 
     public function userExists(string $login): bool
