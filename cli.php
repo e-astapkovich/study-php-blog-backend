8bad10fb6774397511f758b98db392e5dd944f1f
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-05-28 21:50:29 +0500

    в cli.php добавил логер. Исключения теперь записываются в файл, а не выводятся в консоль

--- cli.php
+++ cli.php
@@ -4,13 +4,15 @@
 use Eastap\PhpBlog\Commands\CreateUserCommand;
 use Eastap\PhpBlog\Commands\Arguments;
 use Eastap\PhpBlog\Exceptions\AppException;
+use Psr\Log\LoggerInterface;
 
 require_once __DIR__ . '/bootstrap.php';
 
 $command = $container->get(CreateUserCommand::class);
+$logger = $container->get(LoggerInterface::class);
 
 try {
   $command->handle(Arguments::fromArgv($argv));
 } catch (AppException $e) {
-  echo "{$e->getMessage()}\n";
+  $logger->error($e->getMessage(), ['exception' => $e]);
 }
